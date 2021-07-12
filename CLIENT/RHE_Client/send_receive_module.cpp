0fd1cb47612de271ae273413b15a7712fa0ca0f4
JUGGERNAUT <Poberezsky@gmail.com>
2021-07-12 21:15:34 +0300

    -Added processing with sended/received data in separate class, put in separate thread. -Fixed updating data from server. -UI changes. -Some refactoring.

---
+++ CLIENT/RHE_Client/send_receive_module.cpp
@@ -0,0 +1,6 @@
+#include "tst_send_recv_module.h"
+
+tst_send_recv_module::tst_send_recv_module(QObject *parent) : QObject(parent)
+{
+
+}
---
+++ CLIENT/RHE_Client/send_receive_module.h
@@ -0,0 +1,15 @@
+#ifndef TST_SEND_RECV_MODULE_H
+#define TST_SEND_RECV_MODULE_H
+
+
+class tst_send_recv_module : public QObject
+{
+    Q_OBJECT
+public:
+    explicit tst_send_recv_module(QObject *parent = nullptr);
+
+signals:
+
+};
+
+#endif // TST_SEND_RECV_MODULE_H
